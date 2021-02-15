5dbb1a9f70f48959a65b5ffe62a97b8897c62646
gosvoh <vohmina2011@yandex.ru>
2021-02-15 23:23:12 +0300

    Fixed sort via quick sort

--- src/Week1dot5.java
+++ src/Week1dot5.java
@@ -16,30 +16,49 @@ import java.util.Scanner;
  * TODO Пофиксить этот баг, но я хз как
  */
 public class Week1dot5 {
+    private static FileWriter fileWriter = null;
+
+    public static void quickSort(int[] array, int low, int high) throws IOException {
+        if (low >= high) return;
+
+        int middle = low + (high - low) / 2;
+        int reference = array[middle];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < reference) i++;
+            while (array[j] > reference) j--;
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                if (i != j) fileWriter.write(String.format("Swap elements at indices %d and %d.\n", i + 1, j + 1));
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j) quickSort(array, low, j);
+
+        if (high > i) quickSort(array, i, high);
+    }
+
     public static void main(String[] args) {
         File inFile = new File("input.txt");
         File outFile = new File("output.txt");
 
         try {
             Scanner scanner = new Scanner(inFile);
-            FileWriter fileWriter = new FileWriter(outFile);
+            fileWriter = new FileWriter(outFile);
 
             int count = Integer.parseInt(scanner.nextLine());
             String[] valsStr = scanner.nextLine().split(" ");
             int[] vals = new int[count];
             for (int i = 0; i < vals.length; i++) vals[i] = Integer.parseInt(valsStr[i]);
 
-            for (int i = 1; i < vals.length; i++) {
-                int tmp = vals[i];
-                int j = i;
-                while (j > 0 && tmp < vals[j - 1]) {
-                    vals[j] = vals[j - 1];
-                    j--;
-                }
-                vals[j] = tmp;
-                if (i != j) fileWriter.write(String.format("Swap elements at indices %d and %d.\n", j + 1, i + 1));
-            }
-            System.out.println(Arrays.toString(vals));
+            quickSort(vals, 0, count - 1);
+
             fileWriter.write("No more swaps needed.");
             scanner.close();
             fileWriter.close();
